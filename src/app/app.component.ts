aeb5d5547eaf02edbf186d51f76588a25392ce0f
Jurijs Melniks <jurijs.melniks@28stone.com>
2023-03-30 18:57:40 +0300

    error on importing post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from '.';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-try';
+  myArray = new Array<Post>();
 }
